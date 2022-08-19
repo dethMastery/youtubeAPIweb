5d51c91704ae40a91be89643b7bec869cff9142e
dethMastery <owner@detzz.in.th>
2022-08-19 23:00:09 +0700

    added: volume button

--- data.js
+++ data.js
@@ -46,6 +46,18 @@ document.querySelector('#app').innerHTML = `
           <button id="downTime">-5</button>
           <button id="pBT">Pause</button>
           <button id="upTime">+5</button>
+
+          <br />
+          <br />
+          
+          <button id="loop"></button>
+
+          <br />
+          <br />
+          
+          <button id="volDWN">-</button>
+          <span id="vol"></span>
+          <button id="volUP">+</button>
         </div>
         <br />
       </div>
@@ -72,6 +84,42 @@ axiosCall().then(data => {
   document.querySelector('video').src = data.info.url
   document.querySelector('#download').href = data.info.url
   document.querySelector('#download').download = data.info.display_id + '.mp4'
+
+  // mediaSession
+  if (!isNode) {
+    let title = data.info.title
+    let artist = data.info.channel
+    let cover = data.info.thumbnail
+
+    if ('mediaSession' in navigator) {
+			navigator.mediaSession.metadata = new MediaMetadata({
+				title: `${title}`,
+				artist: `${artist}`,
+				album: `${title}`,
+				artwork: [
+					{ src: `${cover}`, sizes: '96x96', type: 'image/png' },
+					{ src: `${cover}`, sizes: '128x128', type: 'image/png' },
+					{ src: `${cover}`, sizes: '192x192', type: 'image/png' },
+					{ src: `${cover}`, sizes: '256x256', type: 'image/png' },
+					{ src: `${cover}`, sizes: '384x384', type: 'image/png' },
+					{ src: `${cover}`, sizes: '512x512', type: 'image/png' },
+				]
+			})
+		}
+
+		navigator.mediaSession.setActionHandler('play', function() {
+			pl.play()
+		})
+		navigator.mediaSession.setActionHandler('pause', function() {
+			pl.pause()
+		})
+		navigator.mediaSession.setActionHandler('seekbackward', function() {
+			pl.currentTime = pl.currentTime - 5;
+		});
+		navigator.mediaSession.setActionHandler('seekforward', function() {
+			pl.currentTime = pl.currentTime + 5;
+		});
+  }
 })
 
 let pl = document.querySelector('#player')
@@ -83,6 +131,10 @@ let fDur = document.querySelector('#duration')
 let pbt = document.querySelector('#pBT')
 let uTime = document.querySelector('#upTime')
 let dTime = document.querySelector('#downTime')
+
+let volUP = document.querySelector('#volUP')
+let vol = document.querySelector('#vol')
+let volDWN = document.querySelector('#volDWN')
 
 if(!isNode) {
   pl.ontimeupdate = function () {
@@ -157,8 +209,25 @@ if(!isNode) {
   function delTime() {
     pl.currentTime = pl.currentTime - 5
   }
-}
 
-function mediaSession() {
+  volUP.addEventListener("click", vUP)
+  volDWN.addEventListener("click", vDWN)
 
+  function vUP() {
+    pl.volume = pl.volume + 0.1
+    let tvol = Math.floor(pl.volume * 10)
+    let cvol = parseInt(tvol)
+    vol.innerHTML = "Vol : " + cvol
+  }
+
+  function vDWN() {
+    pl.volume = pl.volume - 0.1
+    let tvol = Math.floor(pl.volume * 10)
+    let cvol = parseInt(tvol)
+    vol.innerHTML = "Vol : " + cvol
+  }
+  
+  let tvol = Math.floor(pl.volume * 10)
+  let cvol = parseInt(tvol)
+  vol.innerHTML = "Vol : " + cvol
 }
