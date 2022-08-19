f274a8eb0c5aec7f74db864b155c8a814aa73b5c
dethMastery <owner@detzz.in.th>
2022-08-19 14:13:10 +0700

    added: WIP video controller

--- data.js
+++ data.js
@@ -1,5 +1,12 @@
 import './style.css'
 import axios from 'axios'
+import isNode from 'detect-node'
+
+let url = window.location.href
+let spt = url.split('?')[1]
+let data = spt.split('=')[1]
+
+let api = `https://yt-heroku-detzz.herokuapp.com/api/info?url=${data}`
 
 document.querySelector('#app').innerHTML = `
   <div class="container">
@@ -11,5 +18,92 @@ document.querySelector('#app').innerHTML = `
         Download
       </button>
     </form>
+
+    <br />
+    <br />
+
+    <div class="videoControl">
+      <h3></h3>
+      <br/>
+
+      <video autoplay id="player"></video>
+      <br />
+      
+      <div class="control">
+        <progress id="time" value="0" max="100"></progress>
+      </div>
+
+      <a href="#" id="download" download target="_blank">
+        <button>
+          Download Link
+        </button>
+      </a>
+    </div>
   </div>
 `
+
+function axiosCall() {
+  let axi = axios.get(api)
+  let summon = axi.then((rsp) => rsp.data)
+
+  return summon
+}
+
+axiosCall().then(data => {
+  document.title = data.info.title + " | dethz YT API"
+  document.querySelector('h3').innerHTML = data.info.title
+  document.querySelector('video').src = data.info.url
+  document.querySelector('#download').href = data.info.url
+  document.querySelector('#download').download = data.info.display_id + '.mp4'
+})
+
+function mediaSession() {
+
+}
+
+if(!isNode) {
+  let pl = document.querySelector('#player')
+  let dur = document.querySelector('#time')
+
+  pl.ontimeupdate = function () {
+    var percentage = (pl.currentTime / pl.duration) * 100
+    dur.value = percentage
+
+    // var i = setInterval(function () {
+    //   if (pl.readyState > 0) {
+    //     var fmin = parseInt(pl.duration / 60, 10)
+    //     var fsec = parseInt(pl.duration % 60, 10)
+
+    //     if (fmin < 10) {
+    //       fmin = "0" + fmin
+    //     }
+
+    //     if (fsec < 10) {
+    //       fsec = "0" + fsec
+    //     }
+
+    //     fDur.innerHTML = fmin + ":" + fsec
+
+    //     clearInterval(i)
+    //   }
+    // }, 200)
+
+    // var n = setInterval(function () {
+    //   if (pl.readyState > 0) {
+    //     var cmin = parseInt(pl.currentTime / 60, 10)
+    //     var csec = parseInt(pl.currentTime % 60, 10)
+
+    //     if (cmin < 10) {
+    //       cmin = "0" + cmin
+    //     }
+
+    //     if (csec < 10) {
+    //       csec = "0" + csec
+    //     }
+
+    //     cDur.innerHTML = cmin + ":" + csec
+    //     clearInterval(n)
+    //   }
+    // }, 200)
+  }
+}
